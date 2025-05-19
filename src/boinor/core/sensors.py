2bb4d23e055c9f94119a0b15c4e226276c0c470e
Thorsten Alteholz <boinor@alteholz.dev>
2025-05-19 16:24:47 +0200

    MAINT: take care of non-ascii-name
    non-ascii-names are funny, but hard to type, so I would like to prefer ascii variable names

--- src/boinor/core/sensors.py
+++ src/boinor/core/sensors.py
@@ -3,25 +3,25 @@ import numpy as np
 
 
 @jit
-def min_and_max_ground_range(h, η_fov, η_center, R):
+def min_and_max_ground_range(h, eta_fov, eta_center, R):
     """Calculates the minimum and maximum values of ground-range angles.
 
     Parameters
     ----------
     h : float
         Altitude over surface.
-    η_fov : float
+    eta_fov : float
         Angle of the total area that a sensor can observe.
-    η_center : float
+    eta_center : float
         Center boresight angle.
     R : float
         Attractor equatorial radius.
 
     Returns
     -------
-    Λ_min: float
+    lambdaBig_min: float
         Minimum value of latitude and longitude.
-    Λ_max: float
+    lambdaBig_max: float
         Maximum value of latitude and longitude.
 
     Notes
@@ -30,54 +30,54 @@ def min_and_max_ground_range(h, η_fov, η_center, R):
 
     """
     r_sat = R + h
-    η_max = η_center + η_fov / 2
-    η_min = η_center - η_fov / 2
-    γ_max = np.arcsin(r_sat * np.sin(η_max) / R)
+    eta_max = eta_center + eta_fov / 2
+    eta_min = eta_center - eta_fov / 2
+    gamma_max = np.arcsin(r_sat * np.sin(eta_max) / R)
 
-    if abs(γ_max) <= np.pi / 2:
-        γ_max = np.pi - γ_max
-    γ_min = np.arcsin(r_sat * np.sin(η_min) / R)
-    if abs(γ_min) <= np.pi / 2:
-        γ_min = np.pi - γ_min
+    if abs(gamma_max) <= np.pi / 2:
+        gamma_max = np.pi - gamma_max
+    gamma_min = np.arcsin(r_sat * np.sin(eta_min) / R)
+    if abs(gamma_min) <= np.pi / 2:
+        gamma_min = np.pi - gamma_min
 
     # Maximum and minimum slant ranges
-    ρ_max = R * np.cos(γ_max) + r_sat * np.cos(η_max)
-    ρ_min = R * np.cos(γ_min) + r_sat * np.cos(η_min)
-    Λ_max = np.arcsin(ρ_max * np.sin(η_max) / R)
-    Λ_min = np.arcsin(ρ_min * np.sin(η_min) / R)
+    rho_max = R * np.cos(gamma_max) + r_sat * np.cos(eta_max)
+    rho_min = R * np.cos(gamma_min) + r_sat * np.cos(eta_min)
+    lambdaBig_max = np.arcsin(rho_max * np.sin(eta_max) / R)
+    lambdaBig_min = np.arcsin(rho_min * np.sin(eta_min) / R)
 
-    return Λ_min, Λ_max
+    return lambdaBig_min, lambdaBig_max
 
 
 @jit
-def ground_range_diff_at_azimuth(h, η_fov, η_center, β, φ_nadir, λ_nadir, R):
-    """Calculates the difference in ground-range angles from the η_center angle and the latitude and longitude of the target
-    for a desired phase angle, β, used to specify where the sensor is looking.
+def ground_range_diff_at_azimuth(h, eta_fov, eta_center, beta, phi_nadir, lambda_nadir, R):
+    """Calculates the difference in ground-range angles from the eta_center angle and the latitude and longitude of the target
+    for a desired phase angle, beta, used to specify where the sensor is looking.
 
     Parameters
     ----------
     h : float
         Altitude over surface.
-    η_fov : float
+    eta_fov : float
         Angle of the total area that a sensor can observe.
-    η_center : float
+    eta_center : float
         Center boresight angle.
-    β : float
+    beta : float
         Phase angle, used to specify where the sensor is looking.
-    φ_nadir : float
+    phi_nadir : float
         Latitude angle of nadir point.
-    λ_nadir : float
+    lambda_nadir : float
         Longitude angle of nadir point.
     R : float
         Earth equatorial radius.
 
     Returns
     -------
-    delta_λ : float
+    delta_lambda : float
         The difference in ground-range angles from the eta_center angle.
-    φ_tgt: float
+    phi_tgt: float
         Latitude angle of the target point.
-    λ_tgt: float
+    lambda_tgt : float
         Longitude angle of the target point.
 
     Notes
@@ -85,22 +85,22 @@ def ground_range_diff_at_azimuth(h, η_fov, η_center, β, φ_nadir, λ_nadir, R
     For further information, please take a look at :cite:t:`Vallado2013`, pages 853-860.
 
     """
-    if not 0 <= β < np.pi:
+    if not 0 <= beta < np.pi:
         raise ValueError("beta must be between 0º and 180º")
 
     r_sat = R + h
-    γ = np.arcsin(r_sat * np.sin(η_center) / R)
-    if abs(γ) <= np.pi / 2:
-        γ = np.pi - γ
+    gamma = np.arcsin(r_sat * np.sin(eta_center) / R)
+    if abs(gamma) <= np.pi / 2:
+        gamma = np.pi - gamma
 
-    ρ = R * np.cos(γ) + r_sat * np.cos(η_center)
-    Λ = np.arcsin(ρ * np.sin(η_center) / R)
-    φ_tgt = np.arcsin(
-        np.cos(β) * np.cos(φ_nadir) * np.sin(Λ) + np.sin(φ_nadir) * np.cos(Λ)
+    rho = R * np.cos(gamma) + r_sat * np.cos(eta_center)
+    lambdaBig = np.arcsin(rho * np.sin(eta_center) / R)
+    phi_tgt = np.arcsin(
+        np.cos(beta) * np.cos(phi_nadir) * np.sin(lambdaBig) + np.sin(phi_nadir) * np.cos(lambdaBig)
     )
-    delta_Λ = np.arcsin(np.sin(β) * np.sin(Λ) / np.cos(φ_tgt))
-    λ_tgt = λ_nadir + delta_Λ
-    Λ_min, Λ_max = min_and_max_ground_range(h, η_fov, η_center, R)
-    delta_λ = (Λ_max - Λ_min) / 2
+    delta_lambdaBig = np.arcsin(np.sin(beta) * np.sin(lambdaBig) / np.cos(phi_tgt))
+    lambda_tgt = lambda_nadir + delta_lambdaBig
+    lambdaBig_min, lambdaBig_max = min_and_max_ground_range(h, eta_fov, eta_center, R)
+    delta_lambda = (lambdaBig_max - lambdaBig_min) / 2
 
-    return delta_λ, φ_tgt, λ_tgt
+    return delta_lambda, phi_tgt, lambda_tgt
